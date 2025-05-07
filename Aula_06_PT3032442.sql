ad16ed9bf1c032d99b99c8b4d2515bcce0bd7194
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2025-05-07 19:06:02 -0300

    Update Aula_06_PT3032442.sql

--- Aula_06_PT3032442.sql
+++ Aula_06_PT3032442.sql
@@ -1,9 +1,29 @@
+/* Exercicio 1*/
+create user User_A with password = '@2025a!peloAmor5';
+create user User_B with password = '@2025a!peloAmor1';
+create user User_C with password = '@2025a!peloAmor2';
+create user User_D with password = '@2025a!peloAmor3';
+create user User_E with password = '@2025a!peloAmor4';
+
+/* Exercicio 2*/
+  
+GRANT select, alter on SCHEMA:: dbo TO User_A;
+revoke SELECT ON dbo.classroom from User_A;
+revoke alter ON dbo.classroom from User_A;
 
 
+/* Exercicio 3*/
+SELECT * FROM sysusers;
+select  princ.name
+,       princ.type_desc
+,       perm.permission_name
+,       perm.state_desc
+,       perm.class_desc
+,       object_name(perm.major_id)
+from    sys.database_principals princ
+left join
+        sys.database_permissions perm
+on      perm.grantee_principal_id = princ.principal_id
+where princ.type_desc = 'SQL_USER' and princ.name = 'User_A'
+order by princ.name;
 
-
-
-
-
-
-//E
